d9e56d26f4844653d082cffa0a8ebfffa09d2f3d
manu4195 <133750445+manu4195@users.noreply.github.com>
2025-01-14 13:54:21 +0100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+// DISPLAY start page
+//     SHOW options to Login or Sign Up
+//     SHOW game rules and introduction
+
+
+// RETRIEVE user results
+// DISPLAY results summary
+//     CALCULATE total score
+//     SHOW score and ranking
+//     OFFER option to retry or select another quiz
+
